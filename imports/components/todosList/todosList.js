69b3f3f9e049badef43544c266ebd0cf1b9e59ff
Alex Tulic <alex.tulic@gmail.com>
2017-10-08 15:59:39 +1000

    Initial commit

---
+++ imports/components/todosList/todosList.js
@@ -0,0 +1,36 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import { Meteor } from 'meteor/meteor';
+import { Tasks } from '../../api/tasks.js';
+
+import template from './todosList.html';
+ 
+class TodosListCtrl {
+  constructor($scope) {
+    $scope.viewModel(this);
+ 
+    this.helpers({
+      tasks() {
+        return Tasks.find({});
+      }
+    })
+  }
+  addTask(newTask) {
+    // Insert a task into the collection
+    Tasks.insert({
+      text: newTask,
+      createdAt: new Date
+    });
+ 
+    // Clear form
+    this.newTask = '';
+  }
+}
+ 
+export default angular.module('todosList', [
+  angularMeteor
+])
+  .component('todosList', {
+    templateUrl: 'imports/components/todosList/todosList.html',
+    controller: ['$scope', TodosListCtrl]
+  });
