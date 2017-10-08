56e23a9bb82219028c647a4cf9f91cfb8cb78600
Alex Tulic <alex.tulic@gmail.com>
2017-10-08 17:12:07 +1000

    Added unique user accounts

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -8,10 +8,37 @@ import template from './todosList.html';
 class TodosListCtrl {
   constructor($scope) {
     $scope.viewModel(this);
- 
+
+    this.hideCompleted = false;
+
     this.helpers({
       tasks() {
-        return Tasks.find({});
+
+      const selector = {};
+ 
+        // If hide completed is checked, filter tasks
+        if (this.getReactively('hideCompleted')) {
+          selector.checked = {
+            $ne: true
+          };
+        }
+
+      // Show newest tasks at the top
+        return Tasks.find(selector, {
+          sort: {
+            createdAt: -1
+          }
+        });
+      },
+      incompleteCount() {
+        return Tasks.find({
+          checked: {
+            $ne: true
+          }
+        }).count();
+      },
+      currentUser() {
+        return Meteor.user();
       }
     })
   }
@@ -19,11 +46,25 @@ class TodosListCtrl {
     // Insert a task into the collection
     Tasks.insert({
       text: newTask,
-      createdAt: new Date
+      createdAt: new Date,
+      owner: Meteor.userId(),
+      username: Meteor.user().username
     });
  
     // Clear form
     this.newTask = '';
+  }
+  setChecked(task) {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(task._id, {
+      $set: {
+        checked: !task.checked
+      },
+    });
+  }
+ 
+  removeTask(task) {
+    Tasks.remove(task._id);
   }
 }
  
